3e1e27a0bded006c9f602a40a98b965491214d1d
Meera  Fareena <ajmalfareena2006@gmail.com>
2025-05-10 23:25:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Auto-reload (watch for changes in the 'main.js' and 'renderer' files)
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 450,
+    height: 780,
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
